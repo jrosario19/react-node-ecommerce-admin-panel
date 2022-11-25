a08601853cfca051e19227645bd0291973e31885
Juan Rosario <jrosario19@hotmail.com>
2022-11-24 22:19:02 -0400

    Connect CRUD of product with api call

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrahi_HnHSW9fCW3We0mYUR8QCfcID7Bg",
+  authDomain: "shop-1004d.firebaseapp.com",
+  projectId: "shop-1004d",
+  storageBucket: "shop-1004d.appspot.com",
+  messagingSenderId: "693276401924",
+  appId: "1:693276401924:web:b14b5de0beafe7eeb13051"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,7 +1,15 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux"
 import axios from 'axios'
 import { publicRequest, userRequest } from '../../src/requestMethods'
-import { getProductFailure, getProductStart, getProductSuccess, deleteProductStart, deleteProductSuccess, deleteProductFailure} from "./productRedux";
+import { getProductFailure, getProductStart, 
+        getProductSuccess, deleteProductStart, 
+        deleteProductSuccess, deleteProductFailure,
+        updateProductStart,
+        updateProductSuccess,
+        updateProductFailure,
+        addProductStart,
+        addProductSuccess,
+        addProductFailure} from "./productRedux";
 
 export const login = async (dispatch, user)=>{
     dispatch(loginStart());
@@ -33,5 +41,27 @@ export const deleteProduct = async (id, dispatch)=>{
         dispatch(deleteProductSuccess(id))
     } catch (error) {
         dispatch(deleteProductFailure())
+    }
+}
+
+export const updateProduct = async (id, product, dispatch)=>{
+    dispatch(updateProductStart());
+    try {
+        //const res = await axios.post("https://ecommerce-jfrs.herokuapp.com/api/auth/login",user);
+        const res = await userRequest.delete(`/products/${id}`);
+        dispatch(updateProductSuccess({id, product}))
+    } catch (error) {
+        dispatch(updateProductFailure())
+    }
+}
+
+export const addProduct = async (product, dispatch)=>{
+    dispatch(addProductStart());
+    try {
+        //const res = await axios.post("https://ecommerce-jfrs.herokuapp.com/api/auth/login",user);
+        const res = await userRequest.post(`/products`, product);
+        dispatch(addProductSuccess(res.data))
+    } catch (error) {
+        dispatch(addProductFailure())
     }
 }
